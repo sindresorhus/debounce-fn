4b2b83f36c1d270bc73901dab04a67e414b89007
Sindre Sorhus <sindresorhus@gmail.com>
2019-04-10 10:12:08 +0700

    Meta tweaks

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,11 +1,11 @@
 import {expectType} from 'tsd';
 import debounceFn = require('.');
 
-const debounced = debounceFn((s: string) => true);
-expectType<((s: string) => boolean | undefined) & {cancel(): void}>(debounced);
+const debounced = debounceFn((string: string) => true);
+expectType<((string: string) => boolean | undefined) & {cancel(): void}>(debounced);
 expectType<boolean | undefined>(debounced('foo'));
 debounced.cancel();
 
-debounceFn((s: string) => true);
-debounceFn((s: string) => true, {wait: 100});
-debounceFn((s: string) => true, {immediate: true});
+debounceFn((string: string) => true);
+debounceFn((string: string) => true, {wait: 100});
+debounceFn((string: string) => true, {immediate: true});
